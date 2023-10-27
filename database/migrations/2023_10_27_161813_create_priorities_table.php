a53021a57d028af7d32a7e0700b899cccc150a62
Renan Reis <renanreis.dev@gmail.com>
2023-10-27 13:33:06 -0300

    feat: add migration Priorities and model Priority

---
+++ database/migrations/2023_10_27_161813_create_priorities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('priorities', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->string('color')->default('#FFFFFF');
+            $table->timestamps();
+            $table->foreignIdFor(User::class)->references('id')->on('users')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('priorities', function (Blueprint $table) {
+            $table->dropForeignIdFor(User::class);
+        });
+        Schema::dropIfExists('priorities');
+    }
+};
