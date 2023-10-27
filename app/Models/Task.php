2b372bdb76348f69035b57dc1d15232d05fddb7c
Renan Reis <renanreis.dev@gmail.com>
2023-10-27 13:55:12 -0300

    feat: add Task relationships

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
+use App\Models\Category;
+use App\Models\Priority;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'due_date'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function priority()
+    {
+        return $this->belongsTo(Priority::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
