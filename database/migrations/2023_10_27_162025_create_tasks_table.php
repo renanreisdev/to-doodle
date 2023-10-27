7a55ec5bbdcd33a3b2dd4e6884743b8ab3f6eb37
Renan Reis <renanreis.dev@gmail.com>
2023-10-27 13:33:22 -0300

    feat: add migration tasks and model task

---
+++ database/migrations/2023_10_27_162025_create_tasks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Priority;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->string('description');
+            $table->dateTime('due_date');
+            $table->timestamps();
+            $table->foreignIdFor(Category::class)->references('id')->on('categories')->onDelete('CASCADE');
+            $table->foreignIdFor(Priority::class)->references('id')->on('priorities')->onDelete('CASCADE');
+            $table->foreignIdFor(User::class)->references('id')->on('users')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropForeignIdFor(Category::class);
+            $table->dropForeignIdFor(Priority::class);
+            $table->dropForeignIdFor(User::class);
+        });
+        Schema::dropIfExists('tasks');
+    }
+};
