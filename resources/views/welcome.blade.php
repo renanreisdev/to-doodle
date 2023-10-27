39a8427b6230d8c1ee2bb4aa1b7f62adf04a2c91
Renan Reis <renanreis.dev@gmail.com>
2023-10-27 12:24:51 -0300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>To-Doodle</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1>To-Doodle</h1>
+</body>
+
+</html>
