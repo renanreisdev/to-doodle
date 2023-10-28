f87231da223a76d35b5ea12125f8b00e202b981f
Renan Reis <renanreis.dev@gmail.com>
2023-10-28 20:34:55 -0300

    feat: Add html structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,26 @@ export default {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        fontFamily: {
+            sans: ['Rubik', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                primary: "#1F2833",
+                secondary: "#66FCF1",
+                secondary2: "#45A29E",
+                gray_td: {
+                    100: "#EBF7F7",
+                    200: "#EBECED",
+                    300: "#C5C6C7",
+                },
+                white_td: "#F2F5F7",
+                subtitles: "#C5C6C7"
+            },
+            backgroundImage: {
+                'circle-progress': "conic-gradient(#66FCF1 var(--deg-progress), #F2F5F7 0deg)",
+            }
+        },
     },
     plugins: [],
 }
