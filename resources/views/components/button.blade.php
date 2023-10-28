098424f1568a8a4fd024499c09a1ea3a5e10127e
Renan Reis <renanreis.dev@gmail.com>
2023-10-28 20:55:24 -0300

    feat: add the button as a component

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,4 @@
+<button
+    class="p-3 border border-secondary rounded-md text-white_td text-sm font-medium tracking-wider transition-all ease-linear duration-200 uppercase hover:bg-secondary2">
+    {{ $title ?? 'Button' }}
+</button>
